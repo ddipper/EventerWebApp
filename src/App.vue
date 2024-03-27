4e95b56020902e8375c04f463da90ba086bf63f4
ddipperq <dimonbyt@gmail.com>
2024-03-28 01:51:49 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,47 @@
       </a>
     </div>
   </header>
-  <router-view/>
+  <router-view>
+  </router-view>
+  <footer>
+    <img src="./assets/logoText.png" class="footer-logo">
+    <hr>
+    <div class="footer-content">
+      <div class="footer-copyright">
+        <h3>© 2022 Eventer.by</h3>
+        <h3>Все права защищены</h3>
+      </div>
+      <div class="footer-links">
+        <router-link :to="{ name: 'Coop' }">Корпоративы</router-link>
+        <div class="vl"></div>
+        <router-link :to="{ name: 'BTL' }">BTL</router-link>
+        <div class="vl"></div>
+        <router-link :to="{ name: 'BTL' }">Галерея</router-link>
+        <div class="vl"></div>
+        <router-link :to="{ name: 'BTL' }">Контакты</router-link>
+      </div>
+      <div class="footer-contact">
+        <a href="tel:+375296677654" class="footer-contact-item">
+          <img src="./assets/bwPhone.png">
+          +375 (29) 667 76  54
+        </a>
+        <a href="mailto:eventer@gmail.com" class="footer-contact-item">
+          <img src="./assets/bwMail.png">
+          eventer@gmail.com
+        </a>
+      </div>
+      <div class="footer-social">
+        <a href="t.me_eventer" class="footer-social-item">
+          <img src="./assets/bwTelegram.png">
+          t.me_eventer
+        </a>
+        <a href="inst.com/eventer_by" class="footer-social-item">
+          <img src="./assets/bwInstagram.png">
+          eventer_by
+        </a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
@@ -74,5 +114,60 @@
         }
       }
     }
+  }
+  footer{
+    background-color: #080809;
+    padding: 30px 64px;
+    font-size: 14px;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+
+    hr {
+      background: rgba(255, 255, 255, 0.2);
+      height: 1px;
+      border: none;
+    }
+    
+    a{
+      text-decoration: none;
+      color: #F3F3F3;
+      font-weight: 400;
+    }
+
+    h3, .footer-links a{
+      font-weight: 300;
+      color: rgba(243, 243, 243, 0.6);
+    }
+    
+    .footer-logo {
+      width: 105px;
+    }
+
+    .footer-content{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .footer-copyright, .footer-contact, .footer-social{
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+    }
+    
+    .footer-contact a img, .footer-social a img{
+      margin-right: 8px;
+    }
+
+    .footer-links {
+      display: flex;
+      gap: 40px;
+
+      .vl{
+        height: 16px;
+        border-left: 1px solid rgba(255, 255, 255, 0.2);
+      }
+    }
+
   }
 </style>
