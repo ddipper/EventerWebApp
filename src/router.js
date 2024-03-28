d9714d16b9b154b3517a2ec7cd556f3d5cfc5115
Dmitry <dev.dbuts@gmail.com>
2024-03-28 17:23:44 +0300

    update router.js

--- src/router.js
+++ src/router.js
@@ -3,13 +3,15 @@
 import Coop from './components/Coop.vue'
 import Btl from "./components/Btl.vue"; 
 import Home from "./components/Home.vue";
+import Contacts from "./components/Contacts.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {path: '/', name: 'Home', component: Home},
         {path: '/coop', name: 'Coop', component: Coop},
-        {path: '/btl', name: 'BTL', component: Btl}
+        {path: '/btl', name: 'BTL', component: Btl},
+        {path: '/contacts', name: 'Contacts', component: Contacts}
     ]
 })
 
