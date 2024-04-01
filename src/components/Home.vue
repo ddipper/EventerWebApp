e2137cde8fd8ae907eb6a686985e3b53e193fed0
kolumd21 <kolumd2001@gmail.com>
2024-04-01 11:34:00 +0300

    organizations

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -74,6 +74,38 @@
       </div>
     </div>
   </section>
+  <section id="Organization">
+    <div class="container organization_wrapper">
+      <h2>
+        ЭТАПЫ ОРГАНИЗАЦИИ С EVENTER
+      </h2>
+      <div class="stages">
+        <hr class="stages-line">
+        <div class="stages-points">
+          <div class="point">
+            <div class="first-pt"><h3>1</h3></div>
+            <h3>ВАШ ЗАПРОС</h3>
+          </div>
+          <div class="point">
+            <div class="second-pt"><h3>2</h3></div>
+            <h3>ОБСУЖДЕНИЕ</h3>
+          </div>
+          <div class="point">
+            <div class="third-pt"><h3>3</h3></div>
+            <h3>НАШ РЕСЕРЧ</h3>
+          </div>
+          <div class="point">
+            <div class="fourth-pt"><h3 >4</h3></div>
+            <h3>ПРЕДЛОЖЕНИЕ</h3>
+          </div>
+          <div class="point">
+            <div class="last-pt"><img src="../assets/home/done.png"></div>
+            <h3>ЛУЧШЕЕ МЕРОПРИЯТИЕ НА ВАШ БЮДЖЕТ!</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
   
 </template>
 
@@ -160,117 +192,178 @@
     
   }
   .talant-wrapper{
-      padding: 180px 0;
+    padding: 180px 0;
+    display: flex;
+    justify-content: space-between;
+
+    .talant-left{
       display: flex;
-      justify-content: space-between;
+      flex-direction: column;
+      gap: 24px;
 
-      .talant-left{
-        display: flex;
-        flex-direction: column;
-        gap: 24px;
-
-        img{
-          height: 24px;
-        }
-        h2{
-          font-family: "Roboto Flex";
-          font-weight: 600;
-          font-size: 36px;
-          color: #F3F3F3;
-        }
+      img{
+        height: 24px;
       }
-      .talant-right{
-        width: 636px;
-        display: flex;
-        flex-direction: column;
-        gap: 32px;
+      h2{
+        font-family: "Roboto Flex";
+        font-weight: 600;
+        font-size: 36px;
         color: #F3F3F3;
-
-        .card-wrapper{
-          display: flex;
-          justify-content: space-between;
-          
-          .card{
-            padding: 12px 0;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            background:linear-gradient(to bottom right,#0A0A0B, #0F0F11);
-            gap: 14px;
-            width: calc(33% - 32px);
-
-            p{
-              font-weight: 200;
-              font-size: 14px;
-              color: #F3F3F3;
-            }
-            h3{
-              font-family: "Roboto Flex";
-              font-weight: 600;
-              font-size: 24px;
-              opacity: 0.9;
-            }
-          }
-        }
-        p{
-          font-family: "Manrope";
-          font-weight: 400;
-          font-size: 18px;
-          color: #48484B;
-        }
-
       }
     }
-    #Golden_prison{
-      background-image: url(../assets/home/golden_prison.png);
-      background-size: cover;
-      background-repeat: no-repeat;
-      .container{
-        display: flex;
-        justify-content: right;
-      }
-      .prison-wrapper{
-        display: flex;
-        padding: 140px 0;
-        flex-direction: column;
-        height: 780px;
-        width: 636px;
-        justify-content: space-between;
+    .talant-right{
+      width: 636px;
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+      color: #F3F3F3;
 
-        .prison-text{
+      .card-wrapper{
+        display: flex;
+        justify-content: space-between;
+        
+        .card{
+          padding: 12px 0;
           display: flex;
           flex-direction: column;
-          h2{
-            font-size: 36px;
-            font-weight: 600;
-            line-height: 52px;
-            letter-spacing: .03em;
-            opacity: .9;
-          }
+          align-items: center;
+          background:linear-gradient(to bottom right,#0A0A0B, #0F0F11);
+          gap: 14px;
+          width: calc(33% - 32px);
+
           p{
-            font-size: 18px;
-            font-weight: 400;
+            font-weight: 200;
+            font-size: 14px;
             color: #F3F3F3;
           }
-          p:nth-child(1){
-            display: flex;
-            gap: 2px;
-            align-items: center;
-            margin-bottom: 16px;
-          }
-          p:nth-child(3){
-            margin-top: 8px;
-          }
-          p:nth-child(4){
-            font-weight: 300;
-            letter-spacing: .03em;
-            margin-top: 32px;
+          h3{
+            font-family: "Roboto Flex";
+            font-weight: 600;
+            font-size: 24px;
+            opacity: 0.9;
           }
         }
-        .prison-button{
+      }
+      p{
+        font-family: "Manrope";
+        font-weight: 400;
+        font-size: 18px;
+        color: #48484B;
+      }
+
+    }
+  }
+    
+  #Golden_prison{
+    background-image: url(../assets/home/golden_prison.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+    .container{
+      display: flex;
+      justify-content: right;
+    }
+    .prison-wrapper{
+      display: flex;
+      padding: 140px 0;
+      flex-direction: column;
+      height: 780px;
+      width: 636px;
+      justify-content: space-between;
+
+      .prison-text{
+        display: flex;
+        flex-direction: column;
+        h2{
+          font-size: 36px;
+          font-weight: 600;
+          line-height: 52px;
+          letter-spacing: .03em;
+          opacity: .9;
+        }
+        p{
+          font-size: 18px;
+          font-weight: 400;
+          color: #F3F3F3;
+        }
+        p:nth-child(1){
           display: flex;
-          gap: 16px;
+          gap: 2px;
+          align-items: center;
+          margin-bottom: 16px;
+        }
+        p:nth-child(3){
+          margin-top: 8px;
+        }
+        p:nth-child(4){
+          font-weight: 300;
+          letter-spacing: .03em;
+          margin-top: 32px;
+        }
+      }
+      .prison-button{
+        display: flex;
+        gap: 16px;
+      }
+    }
+  }
+  .organization_wrapper{
+    padding-top: 180px;
+    padding-bottom: 180px;
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+    h2{
+      font-size: 36px;
+      font-weight: 600;
+      color: #F3F3F3;
+      opacity: .9;
+    }
+    .stages{
+      .stages-line{
+        width: 80%;
+        background: linear-gradient(to right,#F86F03,#3E230B 23%,#3E230B 83%,#F86F03);
+        height: 5px;
+        border: none;
+        margin-left: 21px;
+        margin-bottom: -22px;
+      }
+      .stages-points{
+        display: flex;
+
+        .point{
+          width: 20%;
+          div{
+            width: 42px;
+            height: 42px;
+            border-radius: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            h3{
+              color: #F3F3F3;
+              font-size: 20px;
+              font-weight: 600;
+              opacity: .9;
+            }
+          }
+          .first-pt{
+            background: #CF5B00;
+          }
+          .second-pt{
+            background: linear-gradient(#46290F,#321505);
+          }
+          .third-pt{
+            background: linear-gradient(#46290F,#321505);
+          }
+          .fourth-pt{
+            background: linear-gradient(#46290F,#321505);
+          }
+          .last-pt{
+            background: #CF5B00;
+          }
         }
       }
     }
+  }
+    
 </style>
