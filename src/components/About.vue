c8937a31dbf1dfc0075f1a9f608401a4d1efc06c
ddipper <dimonbyt@gmail.com>
2024-04-03 03:29:17 +0300

    fix FormComponent

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<script>
+import ButtonsComponent from './Modules/ButtonsComponent.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+   ButtonsComponent
+  }
+}
+</script>
+
+<template>
+   <ButtonsComponent :color="'blue'" />
+</template>
+
+<style scoped lang="scss">
+
+</style>
