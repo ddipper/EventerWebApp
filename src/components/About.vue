3df011b141fac3f651f69265df6a5ca68af4ccb0
ddipper <dimonbyt@gmail.com>
2024-04-07 21:37:44 +0300

    add carousel

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,17 +1,33 @@
 <script>
 import ButtonsComponent from './Modules/ButtonsComponent.vue';
 import VideoPlayerComponent from './Modules/VideoPlayerComponent.vue';
+import TeamCarouselComponent from './Modules/TeamCarouselComponent.vue';
 
 export default {
   data() {
     return {
-
+      items: Array.from({ length: 12 }, (_, i) => i + 1),
+      currentIndex: 0,
     }
   },
   components: {
     ButtonsComponent,
-    VideoPlayerComponent
-  }
+    VideoPlayerComponent,
+    TeamCarouselComponent
+  },
+  methods: {
+    prev() {
+      this.currentIndex = (this.currentIndex - 1 + this.items.length) % this.items.length;
+    },
+    next() {
+      this.currentIndex = (this.currentIndex + 1) % this.items.length;
+    },
+  },
+  watch: {
+    currentIndex() {
+      this.$refs.track.style.transform = `translateX(-${this.currentIndex * (100 / this.items.length)}%)`;
+    },
+  },
 }
 </script>
 
@@ -30,8 +46,26 @@ export default {
     <VideoPlayerComponent/>
     <section id="bigText">
       <div class="bigText-item">
+        <div class="plus" id="orange"></div>
+        <div class="plus"></div>
+        <h2>11 лет опыта</h2>
+        <div class="plus"></div>
         <div class="plus"></div>
       </div>
+      <div class="bigText-item">
+        <h2>12 креативных людей</h2>
+      </div>
+      <div class="bigText-item">
+        <div class="plus"></div>
+        <div class="plus"></div>
+        <h2>150+<br>мероприятий</h2>
+        <div class="plus" id="blue"></div>
+        <div class="plus"></div>
+      </div>
+    </section>
+    <section id="slider">
+      <h2>Наша команда</h2>
+      <TeamCarouselComponent/>
     </section>
   </div>
 </template>
@@ -104,32 +138,71 @@ export default {
     }
   }
   #bigText{
+    display: flex;
+    padding: 140px 230px;
 
-    .plus {
-      position: relative;
-      width: 30px;
-      height: 30px;
-      background-color: #000;
+    .bigText-item{
+      overflow: visible;
+      font-family: "Roboto Flex";
+      font-weight: 500;
+      font-size: 24px;
+      letter-spacing: 0.03em;
+      text-transform: uppercase;
+      width: 460px;
+      height: 305px;
+      text-align: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
 
-    .plus:before, .plus:after {
-      content: "";
+    .plus {
       position: absolute;
-      background-color: #000;
+      width: 0.8px;
+      height: 64px;
+      background-color: #F3F3F3;
     }
 
     .plus:before {
-      top: 50%;
-      left: 0;
-      width: 100%;
-      height: 100px;
+      content: "";
+      position: absolute;
+      width: 64px;
+      height: 0.8px;
+      background-color: #F3F3F3;
+      left: -31.5px;
+      top: 31.5px;
     }
 
-    .plus:after {
-      top: 0;
-      left: 50%;
-      width: 100px;
-      height: 100%;
+    .bigText-item:nth-child(1), .bigText-item:nth-child(3){
+      position: relative;
+      .plus:nth-child(1){
+        bottom: 0;
+        right: 0;
+      }
+      .plus:nth-child(2){
+        bottom: 0;
+        left: 0;
+      }
+      .plus:nth-child(4){
+        top: 0;
+        right: 0;
+      }
+      .plus:nth-child(5){
+        top: 0;
+        left: 0;
+      }
+      #orange, #orange:before{
+        background-color: #F86F03;
+      }
+      #blue, #blue::before{
+        background-color: #5662DA;
+      }
     }
   }
+
+  #slider{
+    width: 100%;
+    padding: 140px 0;
+  }
+
 </style>
