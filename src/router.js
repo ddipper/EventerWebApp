2c1adc36738be2df1070e7e3c8745394cea95b9d
kolumd21 <kolumd2001@gmail.com>
2024-05-11 14:06:23 +0300

    redy

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Home from "./components/Home.vue";
 import Contacts from "./components/Contacts.vue";
 import About from './components/About.vue';
 import Concept from './components/Concept.vue';
+import Portfolio from './components/Portfolio.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +16,8 @@ const router = createRouter({
         {path: '/btl', name: 'BTL', component: Btl},
         {path: '/contacts', name: 'Contacts', component: Contacts},
         {path: '/about', name: 'About', component: About},
-        {path: '/concept', name: 'Concept', component: Concept}
+        {path: '/concept', name: 'Concept', component: Concept},
+        {path: '/portfolio', name: 'Portfolio', component: Portfolio}
     ]
 })
 
