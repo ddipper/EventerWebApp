3f10e16a63b21658d8d42658c5391950f351c060
ddipper <dimonbyt@gmail.com>
2024-05-11 09:07:54 +0300

    some fix

---
+++ src/components/Btl-case.vue
@@ -0,0 +1,52 @@
+<script>
+
+</script>
+
+<template>
+  <div class="content">
+    <section id="head">
+      <h1>Промоакция</h1>
+      <h2>Coca-cola</h2>
+      <h3>Запрос:</h3>
+      <p>Запросом клиента было мероприятие на свежем воздухе, которое может завлечь все разнообразие сотрудников: программистов от 20 до 57 лет. Что-то еще особенное тут написать, типа сложно было.</p>
+    </section>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .content{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 60px;
+  }
+  #head{
+    width: 1132px;
+    color: #f3f3f3;
+    padding: 120px 0;
+    h1{
+      font-weight: 600;
+      font-size: 40px;
+      padding-bottom: 16px;
+    }
+    h2{
+      font-weight: 500;
+      font-size: 20px;
+    }
+    h3{
+      font-weight: 600;
+      font-size: 20px;
+      color: #ffa41b;
+      padding-top: 20px;
+      padding-bottom: 12px;
+    }
+    p{
+      font-weight: 300;
+      font-size: 16px;
+      color: #f3f3f3;
+      width: 636px;
+      letter-spacing: 0.03em;
+    }
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ import Home from "./components/Home.vue";
 import Contacts from "./components/Contacts.vue";
 import About from './components/About.vue';
 import Concept from './components/Concept.vue';
+import Btl_Case from './components/Btl-case.vue';
+import Coop_Case from './components/Coop-case.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +17,9 @@ const router = createRouter({
         {path: '/btl', name: 'BTL', component: Btl},
         {path: '/contacts', name: 'Contacts', component: Contacts},
         {path: '/about', name: 'About', component: About},
-        {path: '/concept', name: 'Concept', component: Concept}
+        {path: '/concept', name: 'Concept', component: Concept},
+        {path: '/btl-case', name: 'Btl-case', component: Btl_Case},
+        {path: '/coop-case', name: 'Coop-case', component: Coop_Case}
     ]
 })
 
