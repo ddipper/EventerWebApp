7ac18bc36efe65957f9ab0d56fc6c07a5745ef26
kolumd21 <kolumd2001@gmail.com>
2024-05-22 11:39:09 +0300

    start

--- src/components/Coop.vue
+++ src/components/Coop.vue
@@ -1,12 +1,65 @@
-﻿<template>
-  <router-view/>
-  <h1>Coop</h1>
-</template>
+﻿  <template>
+    <router-view/>
+    <section class="hero">
+        <div class="container">
+          <div class="hero-content">
+              <h1>
+                Организация корпоративов<br>
+                в минске            
+              </h1>
+              <h2>
+                Более 180 мероприятий за 10 лет на высшем уровне            </h2>
+              <ButtonsComponent :color="'blue'" />
+          </div>
+        </div>
+    </section>
+  </template>
 
-<script>
+  <script>
+  import ButtonsComponent from './Modules/ButtonsComponent.vue';
+  export default{
+      data(){
+        return{
+        };
+      },
+      components:{
+        ButtonsComponent,
+      },
+    methods: {
+   },
+  }
+  </script>
 
-</script>
+  <style lang="scss" scoped>
+  h1{
+    font-size: 40px;
+    font-weight: 600;
+  }
+  h2{
+    font-size: 20px;
+    font-weight: 500;
+  }
+  .hero{
+    background-image: url(../assets/Corporate/heroCorporate.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  .container{
+    padding: 0 230px;
+  }
+  .hero-content{
+    height: 900px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+    gap: 32px;
 
-<style lang="scss" scoped>
-
-</style>
+    h1{
+      font-size: 40px;
+      font-weight:500;
+      line-height: 60px;
+      color: #F3F3F3;
+    }
+  }
+  </style>
